e3adf6d5a4039f4a715badd1527b346302f550e3
mattsousaa <mateuseng_ec@alu.ufc.br>
2020-07-13 15:12:54 -0300

    Software timer

---
+++ 14Software_Timers/src/main.cpp
@@ -0,0 +1,71 @@
+/*********************************************************
+* Example to demonstrate the usage of the software timer
+* By: Mateus Sousa
+*********************************************************/
+
+#include <Arduino.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+
+#define LED1 2
+#define LED2 14
+#define BT   12
+
+TaskHandle_t xTask1;
+TimerHandle_t xTimer1,xTimer2;
+
+void vTask1(void *pvParametes);
+
+void callBackTimer1(TimerHandle_t xTimer);
+void callBackTimer2(TimerHandle_t xTimer);
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(LED1,OUTPUT);
+  pinMode(LED2,OUTPUT);
+  pinMode(BT,INPUT_PULLUP);
+
+  xTimer1 = xTimerCreate("TIMER1",pdMS_TO_TICKS(1000),pdTRUE,0,callBackTimer1); //Autoreload
+  xTimer2 = xTimerCreate("TIMER2",pdMS_TO_TICKS(10000),pdFALSE,0,callBackTimer2); //OneShot
+
+  xTaskCreate(vTask1,"TASK1",configMINIMAL_STACK_SIZE+1024,NULL,1,&xTask1);
+
+  xTimerStart(xTimer1,0);
+
+}
+
+void loop(){
+  vTaskDelay(pdMS_TO_TICKS(1000));
+}
+
+void vTask1(void *pvParametes){
+
+  uint8_t debouncingTime = 0;
+
+  while(1){
+    if((digitalRead(BT) == LOW) && (xTimerIsTimerActive(xTimer2) == pdFALSE)){
+      debouncingTime++;
+      if(debouncingTime >= 10){
+        debouncingTime = 0;
+        digitalWrite(LED2,HIGH);
+        xTimerStart(xTimer2,0);
+        xTimerStop(xTimer1,0); // Stop blink LED
+        Serial.println("Iniciando o Timer 2 ...");
+      }
+    } else{
+      debouncingTime = 0;
+    }
+    vTaskDelay(pdMS_TO_TICKS(10));
+  }
+}
+
+void callBackTimer1(TimerHandle_t xTimer){
+  digitalWrite(LED1,!digitalRead(LED1));
+}
+
+void callBackTimer2(TimerHandle_t xTimer){
+  digitalWrite(LED2,LOW);
+  xTimerStart(xTimer1,0); // Start blinking LED
+}
